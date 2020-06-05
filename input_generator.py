56f4ed6acc8b3c602740fade98fdc525c3ef46f9
tink-expo <whhan8485@gmail.com>
2020-06-06 02:59:06 +0900

    Refactored file hierachy

--- input_generator.py
+++ input_generator.py
@@ -1,5 +1,5 @@
 import ast, astor
-import sys, os, copy, math, argparse, imp, importlib, time
+import sys, os, copy, math, imp, importlib, time
 import random as rand
 from ast_helper import find_num, find_if, name_len, branch
 
@@ -31,7 +31,7 @@ class InputGenerator:
 
 		self.niter = niter
 		self.lr = lr
-		self.no_cuda = args.no_cuda
+		self.no_cuda = no_cuda
 		self.step_size = step_size
 		self.seed = seed
 		self.model_dir = model_dir
@@ -361,35 +361,6 @@ class InputGenerator:
 		# [generation, total branch, passed branch, time]
 		return rt
 
-
-
-if __name__ == "__main__":
-	parser = argparse.ArgumentParser()
-	parser.add_argument('py_file', type=str, help='Input python function file')
-	parser.add_argument('-p', '--p', type=int, help='Number of population', default=100)
-	parser.add_argument('-g', '--gen', type=int, help='Number of generation', default=1000)
-	parser.add_argument('-pm', '--pm', type=int, help='Probability of mutation in percentage', default=20)
-	parser.add_argument('-ps', '--ps', type=int, help='Percentage of population saved <= 50', default = 10)
-	parser.add_argument('-a', '--alpha', type=int, help='Alpha of gamma distribution', default=1)
-	parser.add_argument('-b', '--beta', type=int, help='Beta of gamma distribution', default=1)
-	parser.add_argument('-f', '--func', type=str, help='Name of revised python file', default='branch_dist_print')
-	parser.add_argument('-br', '--br', type=str, help='Name of branch distance file', default='br_dist')
-
-	# Arguments for our deep learning framework
-	parser.add_argument('--niter', type=int, help="Number of iteration to be optimized", default=10000)
-	parser.add_argument('--lr', type=float, help="Learning for optimizer", default=1e-2)
-	parser.add_argument('--no_cuda', action='store_true', default=False, help='disables CUDA training')
-	parser.add_argument('--step_size',type=float, help="Step size for guided gradient descent", default=0.1)
-	parser.add_argument('--seed', type=int, help='random seed', default=2)
-	parser.add_argument('--model-dir', help="model save directory", default='./ckpt')
-
-	args = parser.parse_args()
-	igen = InputGenerator(
-		args.p, args.gen, args.pm, 
-		args.niter, args.lr, args.no_cuda, 
-		args.step_size, args.seed, args.model_dir
-	)
-	print(igen.test_file(args.py_file))
 	'''
 	root = astor.code_to_ast.parse_file(args.py_file)
 	
